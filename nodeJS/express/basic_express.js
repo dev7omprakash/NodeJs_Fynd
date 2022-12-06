ed45912f6db0bdcce5e5ad281ebc1f2e6bb60b2a
Om prakash <omprakash72810@gmail.com>
2022-12-06 20:36:31 +0530

    creating server using express

--- nodeJS/express/basic_express.js
+++ nodeJS/express/basic_express.js
@@ -17,3 +17,10 @@ app.get("/", function (req, res) {
   console.log("This is the get request from '/' page");
   res.send("Hello, world!");
 });
+
+const server = app.listen(8000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Server is running at ${port}`);
+  console.log(`Server is running at ${host}`);
+});
