7920b5f31aff199b8bf3e9c210c62bef5b982599
Om prakash <omprakash72810@gmail.com>
2022-12-07 18:05:00 +0530

    updating basic express.js

--- nodeJS/express/basic_express.js
+++ nodeJS/express/basic_express.js
@@ -2,17 +2,6 @@ const express = require("express");
 
 const app = express();
 
-/* 1 HTTP Feature
-client-server
-get,post,set,put,delete
-URL Patter(Route) 
-*/
-
-/* express is a minimal and flexible nodejs web application */
-/* The Express philosophy is to provide small, robust tooling for
-HTTP servers, making it a great solution for single page
-applications, websites, hybrids, or public HTTP APIs.*/
-
 app.get("/", function (req, res) {
   console.log("This is the get request from '/' page");
   res.send("Hello, world!");
